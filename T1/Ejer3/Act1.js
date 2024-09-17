c1be9dcc2c71525378abcb76396c29d180511e9d
alumno <alumno@A06-F1-C4.infoxest.net>
2024-09-17 13:56:00 +0200

    Ejer3

---
+++ T1/Ejer3/Act1.js
@@ -0,0 +1,6 @@
+let regex = new RegExp("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
+let cadenaMala = "256.1.1.1";
+let cadenaBuena = "127.0.0.1";
+
+console.log(regex.matches(cadenaMala));     //False
+console.log(regex.matches(cadenaBuena));    //True
