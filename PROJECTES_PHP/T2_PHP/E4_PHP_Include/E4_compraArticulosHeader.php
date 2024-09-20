baf8b3309c6f66849166d3db30ffd1c9f37aeb32
alumno <perellobertetjosepvicent@gmail.com>
2024-09-20 09:32:11 +0200

    E4

---
+++ PROJECTES_PHP/T2_PHP/E4_PHP_Include/E4_compraArticulosHeader.php
@@ -0,0 +1,13 @@
+<?php
+    define("PRECIOFRENOS", 100);
+    define("PRECIOACEITE", 10);
+    define("PRECIORUEDAS", 4);
+    define("IVA", 0.21);
+?>
+<h2>Archivo Header</h2>
+<h4>Precios de los articulos</h4>
+<p>
+    PRECIOFRENOS = <?= PRECIOFRENOS ?><br>
+    PRECIOACEITE = <?= PRECIOACEITE ?><br>
+    PRECIORUEDAS = <?= PRECIORUEDAS ?>
+</p>
---
+++ PROJECTES_PHP/T2_PHP/E4_PHP_Include/E4_ejemplo_include.php
@@ -0,0 +1,6 @@
+<?php
+include ("E4_cabecera.inc");
+echo "Hola";
+include ("E4_pie.inc");
+?>
+
