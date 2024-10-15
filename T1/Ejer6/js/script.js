62c08405c038f50929038eeb6913a5d8e87b7be9
alumno <perellobertetjosepvicent@gmail.com>
2024-10-15 09:04:04 +0200

    add Eliminar o anyadir filas

---
+++ T1/Ejer6/js/script.js
@@ -0,0 +1,54 @@
+let fila;
+
+window.onload = function() {
+    let primeraFila = document.getElementsByTagName("table")[0];
+    cargarEventos(primeraFila);
+    
+    fila = primeraFila.cloneNode(true)
+    let inputs = fila.querySelectorAll("input");
+    inputs.forEach(e => {
+        e.value = "";
+    });
+    
+    mas.addEventListener("click", crearLinea);
+}
+function cargarEventos(fila) {
+    let botones = fila.querySelectorAll(".boton");
+    botones.forEach(e => {
+        if (e.classList.contains("editar")){
+            e.addEventListener("click",habilitarEdicion);
+        } else {
+            e.addEventListener("click", eliminar);
+        }
+    });
+}
+function crearLinea() {
+    let filaNueva = fila.cloneNode(true);
+    main.appendChild(filaNueva);
+    cargarEventos(filaNueva);
+
+}
+function habilitarEdicion() {
+    let padre = this.parentNode.parentNode;
+    let inputs = padre.getElementsByTagName("input");
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].disabled = false;
+    }
+}
+function eliminar() {
+    let eliminar = true;
+    let padre = this.parentNode.parentNode;
+    let inputs = padre.querySelectorAll("input");
+    let nom = padre.querySelectorAll(".nom")[0].value;
+    let ape = padre.querySelectorAll(".ape")[0].value;
+    let dni = padre.querySelectorAll(".dni")[0].value;
+
+    inputs.forEach(e => {
+        if (e.value == "") {
+            eliminar = false;
+        }
+    });
+    if (eliminar) {
+        if(confirm("¿Desea eliminar a " + nom + " " + ape + " con DNI " + dni + "?")) padre.remove(); 
+    }
+}
