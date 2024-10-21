5b4317415874a157bd7e6575bab631f336354f76
alumno <perellobertetjosepvicent@gmail.com>
2024-10-21 12:02:43 +0200

    E3T3

---
+++ PROJECTES_PHP/T3_PHP/E5_EscribFich/E5_formEscribe3.php
@@ -0,0 +1,13 @@
+<?php
+    echo "<h2>Gestión de ficheros en PHP</h2>"
+            . "<b>Genera un fichero .txt en directorio actual</b><br>"
+            . "Version sin comprobación de errores en acceso al archivo<br>"
+            . "El fichero se CREA cada vez que ejecutamos";
+    
+    $ruta = "./E5_petic3.txt";
+    $fitx = fopen($ruta, "a+");
+    if ($fitx) {
+        fwrite($fitx, "Primera línea del saludo.\n<br>Segunda línea del saludo.\n<br>");
+    }
+    fclose($fitx);
+?>
