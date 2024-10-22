29e6d83e810f0956d697a86ad8ab11fea8b603f7
viceentbertet <perellobertetjosepvicent@gmail.com>
2024-10-22 19:43:35 +0200

    finish E5/T3

--- PROJECTES_PHP/T3_PHP/E5_EscribFich/E5_formEscribe3.php
+++ PROJECTES_PHP/T3_PHP/E5_EscribFich/E5_formEscribe3.php
@@ -1,13 +1,15 @@
 <?php
-    echo "<h2>Gestión de ficheros en PHP</h2>"
-            . "<b>Genera un fichero .txt en directorio actual</b><br>"
-            . "Version sin comprobación de errores en acceso al archivo<br>"
-            . "El fichero se CREA cada vez que ejecutamos";
-    
+    echo "<h2>Gestión de ficheros en PHP</h2>";
     $ruta = "./E5_petic3.txt";
     $fitx = fopen($ruta, "a+");
     if ($fitx) {
-        fwrite($fitx, "Primera línea del saludo.\n<br>Segunda línea del saludo.\n<br>");
+        $fecha = getdate()['mday'] . "/" . getdate()['mon'] . "/" . getdate()['year'];
+        $nom = $_GET['nom'];
+        $dir = $_GET['dir'];
+        fwrite($fitx, "$fecha\t$nom\t$dir <br>\n");
+        echo "<b>Se ha escrito el cliente en el fichero</b>";
+    } else {
+        echo "<b>No se ha podido acceder al fichero</b>";
     }
     fclose($fitx);
 ?>
