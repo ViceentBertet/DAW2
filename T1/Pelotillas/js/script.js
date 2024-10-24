57565cf64bb7160a8d14f8f98a26067c8cf4ee51
viceentbertet <perellobertetjosepvicent@gmail.com>
2024-10-24 19:16:53 +0200

    Cambios

--- T1/Pelotillas/js/script.js
+++ T1/Pelotillas/js/script.js
@@ -1,8 +1,8 @@
 let horas = 0;
 let minutos = 0;
 let segundos = 0;
+let nPelotas;
 window.onload = function() {
-    cronometrar();
     pantalla = document.querySelectorAll("input")[0];
     cargarEventos();
 }
@@ -17,17 +17,16 @@ function cargarEventos() {
 /**********************JUGAR***************************/
 
 function jugarJuego() {
-    let nPelotas = obtenerNumero();
-    console.log(nPelotas);
+    cronometrar();
+    nPelotas = obtenerNumero();
+    let opcion = 1;
+    if (!uno.classList.contains("seleccionado")) opcion = 2;
+    centrar.remove();
+    generarPelotas(nPelotas);
+    
 }
 /**********************UTILIDADES***************************/
 
-function obtenerNumero(){
-    let option = document.querySelectorAll("option");
-    let nPelotas;
-    option.forEach(e => {if (e.selected) nPelotas =  e.value;});
-    return nPelotas;
-}
 function sombra() {
     this.classList.toggle("sombra");
 }
@@ -37,6 +36,48 @@ function seleccionar() {
         dos.classList.toggle("seleccionado");
     }
 }
+function obtenerNumero(){
+    let option = document.querySelectorAll("option");
+    let nPelotas;
+    option.forEach(e => {if (e.selected) nPelotas =  e.value;});
+    return nPelotas;
+}
+function generarPelotas(nPelotas) {
+    for (let i = 0; i < nPelotas; i++) {
+        crearPelota();
+    }
+}
+function crearPelota() {
+    let pelotilla = document.createElement("div");
+    pelotilla.classList.add("pelotilla");
+
+    let px = Math.floor(Math.random() * 150);
+    pelotilla.style.width = px + "px";
+    pelotilla.style.height = px + "px";
+    
+    let left = Math.floor(Math.random() * 1400) + 20;
+    let top = Math.floor(Math.random() * 550) + 90;
+    pelotilla.style.left = left + "px";
+    pelotilla.style.top = top + "px";
+    pelotilla.style.borderRadius = "50%";
+    let rojo = 100;
+    let verde = 100;
+    let azul = 100;
+    pelotilla.style.backgroundColor = "red";
+    pelotilla.addEventListener("dblclick", eliminarPelotilla);
+    document.querySelectorAll(".gris")[0].append(pelotilla);
+}
+
+
+function eliminarPelotilla() {
+    marcadorAzul.innerText++;
+    if (marcadorAzul + marcadorRojo == nPelotas) {
+        document.querySelectorAll(".gris")[0].innerText = "Has eliminado las " + nPelotas + "pelotillas en " + cronometro.innerText + "!!"
+        cronometrar();
+    }
+    this.remove();   
+}
+
 /**********************CRONOMETRO***************************/
 //Comienza a cronometrar
 function cronometrar(){
