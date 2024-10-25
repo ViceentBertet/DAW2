95542961ed78cd72a08c35705391bc77d3299266
alumno <perellobertetjosepvicent@gmail.com>
2024-10-25 10:22:24 +0200

    me corro

--- PROJECTES_PHP/T3_PHP/E6_LeeFich/E6_lee4.php
+++ PROJECTES_PHP/T3_PHP/E6_LeeFich/E6_lee4.php
@@ -1,1 +1,15 @@
 <?php
+echo "<b>utiliza fpassthru()</b><br><br>
+      <b>Visualizamos el contenido del fichero mensajes4.txt</b><br><br>
+      <ul><li>Archivo completo mediante readfile()</li>
+      <li>La primera línea en negrita utilizando función open() y fgets()</li>
+      <li>Volcar el resto del fichero salida estándar con fpasthru()</li></ul>";
+$ruta = "./ficheros/E6_mensajes4.txt";
+if (file_exists($ruta)) {
+    $gestor = fopen($ruta, "r");
+    echo "Visualizamos con readfile()<br>";
+    readfile($ruta);
+    echo "<b>" . fgets($gestor) . "</b><br><br>";
+    fpassthru($ruta);
+    fclose($gestor);
+} else echo "No existe $ruta";
