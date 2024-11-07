621d7ac6b5781f4a4acd7bbac6b141a00110f7eb
viceentbertet <perellobertetjosepvicent@gmail.com>
2024-11-07 18:41:52 +0100

    add Buscaminas terminado

--- T1/Buscaminas/js/scripts.js
+++ T1/Buscaminas/js/scripts.js
@@ -1,21 +1,21 @@
 let nBombas;
 let casillas = [];
+let acumulador = 0;
+let clicks = 0;
 window.onload = function() {
-    cargarEventos();
-}
-function cargarEventos() {
     empezar.addEventListener('click', jugar);
 }
+
 function jugar() {
     if (numMinas.value < 5 || numMinas.value > 50) error.innerText = "Tiene que ser un valor entre 5 y 50"
     else {
         nBombas = numMinas.value;
-        borrarMenu();
+        borrarTablero();
         dibujarTablero();
         rellenarTablero();
     }
 }
-function borrarMenu() {
+function borrarTablero() {
     let divs = tablero.querySelectorAll('div');
     divs.forEach(div => {
         div.remove();
@@ -30,15 +30,11 @@ function dibujarTablero() {
         tablero.appendChild(hijo);
     }
     casillas = tablero.getElementsByTagName("div");
-    
 }
 function rellenarTablero(){
     let matriz = Array.from({ length: 10 }, () => Array(10).fill(0));
     let posBombas = generarPosBombas();
     sustituirPosiciones(matriz,posBombas);
-    for (let fila of matriz) {
-        console.log(fila);
-    }
     volcarTablero(matriz);
 }
 function generarPosBombas() {
@@ -89,10 +85,25 @@ function sustituirPosiciones(matriz, posBombas){
     añadiendo las clases correspondientes
 */
 function volcarTablero(matriz) {
-    console.log(casillas);
     for (let i = 0; i < 10; i++) {
-        for (let j = 0; i < 10; j++){
-            
+        for (let j = 0; j < 10; j++){
+            let content = matriz[i][j];
+            /*
+            Con la siguiente operación conseguimos saber que casilla 
+            es la correspondiente con la matriz
+            */
+            let posCasillas = i * 10 + j;
+            if (content == "x") {
+                casillas[posCasillas].classList.add("mina");
+            } else if (content != 0) {
+                if (content == 1)
+                    casillas[posCasillas].classList.add("poco");
+                else if (content == 2)
+                    casillas[posCasillas].classList.add("medio");
+                else 
+                    casillas[posCasillas].classList.add("mucho");
+                casillas[posCasillas].innerText = content;
+            }
         }
     }
 }
@@ -101,5 +112,56 @@ function isX(n) {
     return false;
 }
 function buscarMina() {
-    this.classList.remove('oculto');
+    if (this.classList.contains('oculto')) {
+        clicks++;
+        this.classList.remove('oculto');
+        if (!this.classList.contains("mina")) {
+            let puntoActual = parseInt(this.innerText);
+            if (isNaN(parseInt(this.innerText))) puntoActual = 0;
+            acumulador += (puntoActual + 1) * nBombas;
+            puntos.innerText = acumulador + "\npuntos"; 
+        } else {
+            pantallaFinal(false); 
+        }
+        
+        if (clicks == (100 - nBombas)) {
+            pantallaFinal(true);
+        }
+    }
+}
+function pantallaFinal(ganado) {
+    let mensaje = "Has ganado con ";
+    protector.classList.remove("ocultar");
+    mensajeFinal.classList.remove("ocultar");
+    if (!ganado)
+        mensaje = "Has perdido con ";
+        
+    mensajeFinal.innerText = mensaje + puntos.innerText;
+    let br = document.createElement("br");
+    let boton = document.createElement("button");
+    boton.id = "vuelta";
+    boton.innerText = "Volver a jugar";
+    mensajeFinal.appendChild(br);
+    mensajeFinal.appendChild(boton);
+    vuelta.addEventListener("click", volverJugar);
+}
+function volverJugar(){
+    acumulador = 0;
+    clicks = 0;
+    protector.classList.add("ocultar");
+    mensajeFinal.classList.add("ocultar");
+    borrarTablero();
+    let div1 = document.createElement("div");
+    let div2 = document.createElement("div");
+    let input = document.createElement("input");
+    input.type = "number";
+    input.id = "numMinas";
+    let boton = document.createElement("button");
+    boton.id = "empezar";
+    boton.innerText = "Empezar";
+    boton.addEventListener('click', jugar);
+    div1.appendChild(input);
+    div2.appendChild(boton);
+    tablero.appendChild(div1);
+    tablero.appendChild(div2);
 }
