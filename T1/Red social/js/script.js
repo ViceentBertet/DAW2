e8cf181fe56e52ea1b6904948b4cc861a1a6526c
alumno <perellobertetjosepvicent@gmail.com>
2024-12-16 14:48:31 +0100

    Acts

---
+++ T1/Red social/js/script.js
@@ -0,0 +1,5 @@
+const API_USERS = "https://jsonplaceholder.typicode.com/users";
+const API_GENERO = "https://api.genderize.io?name=";
+const API_USERS = "";
+const API_USERS = "";
+const API_USERS = "";
