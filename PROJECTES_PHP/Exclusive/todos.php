9a039fec1a46697e877c673ce2b07c22698150c1
Josep Vicent Perelló Bertet <perellobertetjosepvicent@gmail.com>
2025-01-27 19:07:31 +0100

    add responsive productos

--- PROJECTES_PHP/Exclusive/todos.php
+++ PROJECTES_PHP/Exclusive/todos.php
@@ -1,36 +1,35 @@
 <?php
-    include("header.php");
-    $hostname = 'localhost';
-    $usuario = 'root';
-    $pwd = '';
-    $database = 'exclusivebddpruebas';
-    $port = 3306;
-    $cadena_conexion = "mysql:host=$hostname;dbname=$database;port=$port;";
-    try {
-        $pdo = new PDO($cadena_conexion, $usuario, $pwd);
-        $query = 'SELECT * FROM producto';
-        $stmt = $pdo->prepare($query, [PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL]);
-        $stmt->execute();
-        $n_filas = $stmt->rowCount();
+include("header.php");
+$hostname = 'localhost';
+$usuario = 'root';
+$pwd = '';
+$database = 'exclusivebddpruebas';
+$port = 3306;
+$cadena_conexion = "mysql:host=$hostname;dbname=$database;port=$port;";
+try {
+    $pdo = new PDO($cadena_conexion, $usuario, $pwd);
+    $query = 'SELECT * FROM producto';
+    $stmt = $pdo->prepare($query, [PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL]);
+    $stmt->execute();
+    $n_filas = $stmt->rowCount();
 ?>
-    <p>Productos encontrados: <?=$n_filas?></p>
+    <p id="nProd">Productos encontrados: <?=$n_filas?></p>
     <div class='productos'>
 <?php
-        while ($registro = $stmt->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT)) {
+    while ($registro = $stmt->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT)) {
 ?>
     <div>
         <h3><?=$registro['nom']?></h3>
         <img src="<?=$registro['img']?>" alt="<?=$registro['nom']?>">
-        <p><?=$registro['precio']?> €</p>
-        <p><?=$registro['descrip']?></p>
-
+        <p class="descrip"><?=$registro['descrip']?></p>
+        <p class="precio"><?=$registro['precio']?> €</p>
      </div>
 <?php
-        }
-        echo "</div>";
-        $pdo = null; //Así se cierra la conexión
-    } catch (PDOException $e) {
-        echo "Error con la base de datos: <b>$database</b><br>" . $e->getMessage(); 
     }
-    include("footer.php");
+    echo "</div>";
+    $pdo = null; //Así se cierra la conexión
+} catch (PDOException $e) {
+    echo "Error con la base de datos: <b>$database</b><br>" . $e->getMessage(); 
+}
+include("footer.php");
 ?>
